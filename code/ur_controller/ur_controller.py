54c7d2939e924e1c159900966ac2ba7c44d22b2e
Ibrahim Haj Yousef <ibyou18@student.sdu.dk>
2023-10-12 14:25:17 +0200

    add_urtde

--- code/ur_controller/ur_controller.py
+++ code/ur_controller/ur_controller.py
@@ -1,6 +1,6 @@
 import time
 import logging
-
+import sys
 from rtde_control import RTDEControlInterface as RTDEControl
 from rtde_receive import RTDEReceiveInterface as RTDEReceive
 from rtde_io import RTDEIOInterface as RTDEIO
@@ -12,27 +12,67 @@ D_OUT_GRIPPER = 4
 class URController(object):
     """Class for controlling the UR robot
     """
-
-    # Logger for URController
-    logger = logging.getLogger("URController")
-
     # Ranges for x, y, z coordinates
-    x_range = (-0.05, -0.50)  # (m)
-    y_range = (-0.69, -1.08)  # (m)
-    z_range = (-0.28, 0.0, 0.15)  # (m)
+    x_range = (-0.95, -0.3)  # (m) -300 mm to -950 mm
+    y_range = (-0.765, -0.05)  # (m) -100 mm to -765 mm 
+    z_range = (0.125, 0.2, 0.55)  # (m) 125 250 to 550
 
     # Home position
-    home = (-0.30, -0.25, 0.025)  # (m), (x, y, z)
-
+    home = (-0.35, -0.1, 0.25, 1.826, 2.555 ,0.0)  # (m), (x, y, z, rx, ry, rz)
+    ORIENTATION:list = [1.826, 2.555 ,0.0]  # (rx, ry, rz)
+    VELOCITY:float = 0.1
+    ACCELERATION:float = 0.1
+    
     def __init__(self) -> None:
         """Constructor for URController
         """
+        # Logger for URController
+        fh = logging.FileHandler("log/ur_controller.log", mode="w")
+        log_level: int = logging.DEBUG
+        fh.setLevel(log_level)
+        fh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+        fh.set_name("URController")
+        
+        self.logger = logging.getLogger("URController")
+        self.logger.setLevel(log_level)
+        self.logger.addHandler(fh)
+
+
         self.logger.info("URController init")
+        IP = "192.168.100.10"
+       
+        try:
+            self.rtde_c = RTDEControl(IP)
+            self.rtde_r = RTDEReceive(IP)
+            self.rtde_io = RTDEIO(IP)
+        except RuntimeError:
+            time.sleep(1.0)
+            self.logger.debug("Retrying to connect to the robot")
+            self.rtde_c = RTDEControl(IP)
+            self.rtde_r = RTDEReceive(IP)
+            self.rtde_io = RTDEIO(IP)
+        
+        self.logger.info("URController connected to the robot")
 
     def __del__(self) -> None:
         """Destructor for URController
         """
-        self.logger.info("URController del")
+        # Robot
+        try:
+            # Stop the robot controller
+            self.rtde_c.speedStop()
+            self.rtde_c.stopScript()
+            self.rtde_c.disconnect()
+            self.rtde_io.disconnect()
+        except:
+            print("Robot failed to terminate")
+
+        try:
+            # Disconnect the receiver
+            self.rtde_r.disconnect()
+        except:
+            print("Robot failed to terminate")
+        
 
     def _assert_range(self, x: float = None, y: float = None, z: float = None) -> bool:
         """Asserts that the given x, y, z coordinates are within range
@@ -78,11 +118,15 @@ class URController(object):
         """
         self.logger.info("URController calibrate_force_sensor()")
 
-        time.sleep(2.0)
+        time.sleep(0.2)
+        if not self.rtde_c.zeroFtSensor():
+            self.logger.error("Failed to zero force sensor")
+            return False
+        time.sleep(0.2)
 
-        # TODO calibrate
+        self.logger.info("Force sensor calibrated")
 
-        time.sleep(2.0)
+        # Force sensor calibrated
         return True
 
     def digital_out(self, pin: int, value: bool, sleep_s: float = 1.0) -> bool:
@@ -97,10 +141,15 @@ class URController(object):
             bool: True if successful, False otherwise
         """
         self.logger.info(f"URController digital_out({pin}, {value})")
-
-        # TODO set value
+        if not self.rtde_io.setStandardDigitalOut(pin, value):
+            self.logger.error(f"Failed to set digital output {pin} to {value}")
+            return False
 
         time.sleep(sleep_s)
+
+        self.logger.info(f"URController digital_out({pin}, {value}) finished")
+
+        # Digital output set
         return True
 
     def move_height(self, z: float, force: float = None) -> bool:
@@ -116,15 +165,42 @@ class URController(object):
         self.logger.info(f"URController move_z({z})")
 
         # Assert z is within range
-        if not self._assert_range(z=z):
+        x, y, z_, _,_,_ = self.rtde_r.getActualTCPPose()
+        if not self._assert_range(z=z_):
             return False
 
-        # TODO move
+        # Make the movement assync if force is given
+        assync = False
+        if force is not None:
+            assync = True
+
+        # Define the position
+        pos = [x, y, z] + self.ORIENTATION
+
+        # Move to the given z coordinate
+        self.rtde_c.moveL(pos, self.VELOCITY, self.ACCELERATION, assync)
 
         if force is not None:
-            pass
+            while True:
+                force_tcp = self.rtde_r.getActualTCPForce()
+                if force_tcp[2] > force:
+                    self.logger.info(f"Force {force_tcp[2]} N reached")
+                    break
 
+                # Validate the distance between the current z and the target z
+                _, _, z_, _,_,_ = self.rtde_r.getActualTCPPose()
+                if abs(z - z_) < 0.0001: # 0.1 mm
+                    self.logger.warning(f"Pose reached with force {force_tcp[2]} N")
+                    break
+
+            # Stop the robot movement
+            self.rtde_c.stopL(0.1)
+        
+        self.logger.info(f"URController move_z({z}) finished")
+
+        # Reached the target z
         return True
+    
 
     def move_to_home(self) -> bool:
         """Moves the robot to the home position
@@ -135,11 +211,16 @@ class URController(object):
         self.logger.info("URController move_to_home()")
 
         # Assert z is within range
-        x, y, z = 0  # TODO read from the robot
+        x, y, z, _,_,_ = self.rtde_r.getActualTCPPose()
+     
         if not self._assert_range(x=x, y=y, z=z):
             return False
 
-        # TODO move
+        self.rtde_c.moveL(self.home, self.VELOCITY, self.ACCELERATION)
+
+        self.logger.info("URController move_to_home() finished")
+
+        # Reached the home position
         return True
 
     def move_to_object(self, x: float, y: float, z: float = None) -> bool:
@@ -157,31 +238,42 @@ class URController(object):
 
         if z is None:
             # TODO assert height before moving to x, y
-            z = 0  # TODO read z from the robot
+            _, _, z, _,_,_ = self.rtde_r.getActualTCPPose()
 
         # Assert x, y are within range
         if not self._assert_range(x=x, y=y, z=z):
             return False
 
-        # TODO move
+        pos = [x, y, z] + self.ORIENTATION
+        self.rtde_c.moveL(pos, self.VELOCITY, self.ACCELERATION)
+
+        self.logger.info(f"URController move_to_object({x}, {y}, {z}) finished")
+
+        # Reached the target x, y
         return True
 
 
 if __name__ == "__main__":
+    print("URController started!")
     # Universal Robots controller
     ur_controller = URController()
 
+
+    
     # Test digital output
     ur_controller.digital_out(pin=D_OUT_GRIPPER, value=True)
     ur_controller.digital_out(pin=D_OUT_GRIPPER, value=False)
-
+    
     ur_controller.move_to_home()
+    
+    ur_controller.calibrate_force_sensor()
 
-    ur_controller.move_height(z=-0.2)
-    ur_controller.move_height(z=0.0)
-    ur_controller.move_height(z=-0.2, force=3.0)
-    ur_controller.move_height(z=0.0)
+    ur_controller.move_to_object(x=-0.5, y=-0.5)
 
-    ur_controller.move_to_object(x=-0.75, y=0.0)
+    ur_controller.move_height(z=0.13)
+    ur_controller.move_height(z=0.25)
+    ur_controller.move_height(z=0.13, force=3.0)
+    ur_controller.move_height(z=0.25)
+
 
     ur_controller.move_to_home()
